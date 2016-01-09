26a844912932dfcd8082e89e7e4317714734f657
Yan Li <mangakjd@gmail.com>
2016-01-08 19:41:01 -0500

    adds manga-cat cli, scraping/downloading functionality

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# manga-cat
-scrapes and downloads manga from MangaStream, like a cat, Javascript-style
+# MangaCat ヽ(^‥^=ゞ)
+MangaCat is a Node.js CLI (command line interface) that scrapes and downloads manga from [MangaStream](http://mangastream.com/), like a cat, Javascript-style. This project is purely educational purposes and serve as a pet project for me to learn more about Javascript and Node.
+
+**Please support MangaStream for bringing high-quality manga and the authors who wrote the manga you're reading.**
+
+##Requirements
+- Node.js **v4.0+** - http://nodejs.org/
+- npm **v2.0+** - https://www.npmjs.org/
+
+##Installation
+`npm install mangacat -g`
+
+##Usage
+```
+Usage: mangacat [options]
+
+Options:
+
+  -h, --help            output usage information
+  -V, --version         output the version number
+  -l, --list            output full manga list information
+  -r, --releases        output latest releases information
+  -d, --download <url>  download all available chapters from given url (e.g. http://mangastream.com/manga/toriko)
+```
+
+Downloads will appear in a `downloads` directory within `manga-cat`.
+
+You can also combine `-l` and `-r` commands with `grep` to search for certain titles in the manga or releases list. By default, they return the full list.
+
+For example: `mangacat -r | grep -A 2 -i 'air gear'` will narrow down your search to returning the title, 'Air Gear' and displaying the next 2 lines after the match.
+
+##Development
+1. `git clone https://github.com/yanandcoffee/manga-cat.git`
+2. `cd manga-cat && npm install`
+3. `npm link` and you can use start using `mangacat` in terminal
+
+Now you can develop locally by changing the `index.js` script and run all mangacat commands in your terminal.
+
+##Issues
+If you find any bugs, please report them in the [Issues](https://github.com/yanandcoffee/manga-cat/issues) section.
+
+
+
---
+++ index.js
@@ -0,0 +1,109 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const program = require('commander');
+const Xray = require('x-ray');
+const Download = require('download');
+const x = new Xray();
+
+program
+  .version('0.1.0')
+  .option('-l, --list', 'output full manga list information (title, url)', getFullMangaList)
+  .option('-r, --releases', 'output latest releases information (title, latest chapter, url)', getLatestReleaseList)
+  .option('-d, --download <url>', 'download all available chapters from given url (e.g. http://mangastream.com/manga/toriko)')
+  .parse(process.argv);
+
+if (program.download) getChaptersFrom(program.download);
+
+process.on('uncaughtException', (err) => {
+  console.log(err);
+});
+
+function getFullMangaList() {
+  let manga_list_url = 'http://mangastream.com/manga';
+
+  x(manga_list_url, '.main-body .table strong a',
+    [{
+      title: '',
+      url: '@href'
+    }]
+  )(function(err, results) {
+    console.log(results);
+  });
+}
+
+function getLatestReleaseList() {
+  let manga_list_url = 'http://mangastream.com/manga';
+
+  x(manga_list_url, '.main-body .table tr',
+    [{
+      title: 'strong a',
+      latest: 'a.chapter-link',
+      url: '.chapter-link@href'
+    }]
+  )((err, results) => {
+    if (err) throw err;
+    console.log(results);
+  });
+}
+
+function getChaptersFrom(url) {
+  const getChapters = new Promise((resolve, reject) => {
+    x(url, '.main-body', {
+      title: 'h1',
+      chapters: x('.table a', [{
+        name: '',
+        url: '@href'
+      }])
+    })((err, result) => {
+      if (err) {
+        reject(Error(err));
+      } else {
+        resolve(result);
+      }
+    });
+  });
+
+  getChapters.then((results) => {
+    console.log('Downloading...');
+    downloadChapters(results);
+  });
+}
+
+function downloadChapters(chapter_list) {
+  chapter_list.chapters.forEach((chapter) => {
+    let url = chapter.url;
+    let length = 0;
+
+    const getLength = new Promise((resolve, reject) => {
+      x(url, '.main-body .btn-group:last-child .dropdown-menu li:last-child a')((err, result) => {
+        if (err) {
+          reject(Error(err));
+        } else {
+          let regExp = /\(([^)]+)\)/g;
+          let matches = regExp.exec(result);
+          length = matches[1];
+          resolve(result);
+        }
+      });
+    });
+
+    getLength.then((result) => {
+      // remove last digit from http://readms.com/r/platinum_end/001/3013/1
+      let sliced_url = url.slice(0, -1);
+
+      for (let i = 1; i < length; i++) {
+        x(sliced_url + i, 'img#manga-page@src')((err, image) => {
+          if (err) throw err;
+
+          let download = new Download();
+          download.get(image);
+          download.dest('./downloads/' + chapter_list.title + ' - Chapter ' + chapter.name);
+          download.run();
+          console.log(chapter_list.title + ' - Chapter ' + chapter.name + ' - Page ' + i)
+        });
+      }
+    });
+  });
+}
